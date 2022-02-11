2871e5b56b900527228364a1d6c8f357c01214c0
LizzieCernaa <98780118+LizzieCernaa@users.noreply.github.com>
2022-02-11 10:43:27 -0600

    Ingrese datos

---
+++ AreaDeUnCriculo/AreaDeUnCriculo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaDeUnCriculo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que haya el area de un circulo" );
+
+            Console.WriteLine("Ingresea el radio:");
+            int r = int.Parse( Console.ReadLine() );
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
